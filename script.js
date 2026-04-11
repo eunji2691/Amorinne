d040f7355c09a9ffab20c233ddee584c9532011d
eunji2691 <eunji2691@naver.com>
2026-04-11 23:27:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const STUDIO_ONLY_TABLES = [
     id: "yeonhwa",
     name: "프리미엄 연화상",
     desc: "스튜디오 전용으로 준비된 프리미엄 전통 상차림",
-    img: "images/yunwha.jpg.JPG",
+    img: "images/yunwha.jpg",
     studioPrice: 85000
   }
 ];
