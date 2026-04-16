b192291dbd85c45174bf3f313bc1ba6ce23ca9c7
eunji2691 <eunji2691@naver.com>
2026-04-17 00:18:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const USE_FAKE_SUBMIT = false;
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz3u-ckGI7KD2cnSuHUVYqx18f60RDyQo2HIew1r6x1_hoKkTvItvCPgz4CB5sBQ3f_3Q/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwy56wNtc6AnFBJVYNzwsnIsa3swWDk2Mrr6u838apohRgpOp6DtZySB84px1-KRUQVBg/exec';
 const KAKAO_CHAT_URL = 'http://pf.kakao.com/_cxhePn/chat';
 
 
